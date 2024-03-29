6c83a3b8855f3f630f3d232da3d8cf193bc778f0
Ramses Llamosas <gonzalezllamosas07@gmail.com>
2024-03-29 15:33:09 -0600

    Socket con funcionalidad tipo ECO

---
+++ src/client.java
@@ -0,0 +1,42 @@
+//Importación de bibliotecas
+import java.net.*;
+import java.io.*;
+
+public class client {
+    public static void main(String args[]){
+        //Se crea un bloque try-catch y se define un flujo de lectura de la entrada estándar
+        try{
+            BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+
+            //Se obtiene la dirección y puerto del servidor del usuario
+            System.out.println("Escriba la dirección del servidor: ");
+            String host = br1.readLine();
+            System.out.printf("\n\nEscriba puerto: ");
+            int pto = Integer.parseInt(br1.readLine());
+
+            //Se crea el socket
+            Socket cl = new Socket(host, pto);
+
+            //Se crea un flujo de carácter ligado al socket para recibir el mensaje
+            BufferedReader br2 = new BufferedReader(new InputStreamReader(cl.getInputStream()));
+
+            //Lee el mensaje recibido por el servidor
+            String mensaje = br2.readLine();
+            System.out.println("Recibimos un mensaje desde el servidor");
+            System.out.println("Mensaje: " + mensaje);
+
+            //Se define el mismo mensaje enviar y ligamos un PrintWriter a un flujo de salida de carácter
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(cl.getOutputStream()));
+            pw.println(mensaje);
+            pw.flush();
+
+            //Se cierra los flujos, el socket y finaliza el programa
+            br1.close();
+            br2.close();
+            pw.close();
+            cl.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/server.java
@@ -0,0 +1,45 @@
+//Importación de bibliotecas
+import java.net.*;
+import java.io.*;
+
+
+public class server {
+    public static void main(String args[]){
+
+        try {
+            //Obtenemos la dirección del servidor en terminal
+            InetAddress direccionServidor = InetAddress.getLocalHost();
+            System.out.println("Dirección del servidor: " + direccionServidor.getHostAddress());
+
+            //Se define la clase ligada al puerto 1234
+            ServerSocket s = new ServerSocket(1234);
+            System.out.println("\n\nEsperando cliente...");
+
+            //El servidor espera dentro infinito la solicitud de conexión de un cliente
+            for(;;){
+                Socket cl = s.accept();
+                System.out.println("Conexión establecida desde " + cl.getInetAddress() + ":" + cl.getPort());
+
+                //Se define el mensaje enviar y ligamos un PrintWriter a un flujo de salida de carácter
+                String mensaje = "Hola mundo";
+                PrintWriter pw = new PrintWriter(new OutputStreamWriter(cl.getOutputStream()));
+                pw.println(mensaje);
+                pw.flush();
+
+                //Se crea un flujo de carácter ligado al socket para recibir el mensaje
+                BufferedReader br1 = new BufferedReader(new InputStreamReader(cl.getInputStream()));
+
+                //Lee el mensaje recibido por el cliente
+                String mensajeServidor = br1.readLine();
+                System.out.println("Recibimos un mensaje desde el cliente");
+                System.out.println("Mensaje: " + mensajeServidor);
+
+                //Se cierra el flujo, el socket, el ciclo for y el bloque try-catch
+                pw.close();
+                cl.close();
+            }//for
+        } catch (Exception e){
+            e.printStackTrace();
+        }//catch
+    }//main
+}
